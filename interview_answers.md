04b050efc77e01f414e3ed196e9bc544ff43d617
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-15 19:21:18 -0400

    written complete. testing incomplete

--- interview_answers.md
+++ interview_answers.md
@@ -2,9 +2,16 @@
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. These will not be counted as a part of your sprint score but will be helpful for preparing you for your endorsement interview, and enhancing overall understanding.
 
 1. Explain what a token is used for.
+    A token is an authorization tool. with it an authenticated user can navigate with permissions while a a non authenticated user would be held back at login or just pages that don't require authentication
 
-2. What steps can you take in your web apps to keep your data secure?
+2. What steps can you take in your web apps to keep your data  secure?
+    password and username authentication. Generating tokens and also deleteing them on logout for active users. Saving and uploading state and data from a 3rd party external API.
 
 3. Describe how web servers work.
+    Servers are computers or programs that run websites. They store and deliver sites and data, with special permissions/requests required.
 
 4. Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+  C .post will add data to an API/Server.
+  R .get will recieve data for view.
+  U .put will uipdate data objects.
+  D .delete will delete specific data objects.
--- src/components/View.test.js
+++ src/components/View.test.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
+import {articleService as mockArticleService} from '../services/articleServices';
+jest.mock('../services/articleServices')
+
+const testArticlesNone=[]
+const testArticles=[
+    {
+        id: 5,
+        headline: "Testing",
+        createdOn: '11/11/11',
+        author:"beginner",
+        image: 134,
+        summary: "Triple-digit temperatures led to a spike in demand across the region.",
+        body: "Inside the attic of a one-story gray house in a Seattle suburb last week, Jeff Bryson gingerly strapped copper piping across the rafters while wearing a white face mask and a headlamp. The temperature was about 110 degrees in the tight space, which was covered in insulation dust. His work was meant to cool the rest of the home."   
+    },
+    {
+        id: 6,
+        headline: "Testing1",
+        createdOn: '11/12/11',
+        author:"beginner1",
+        image: 134,
+        summary: "Triple-digit temperatures led to a spike in demand across the region.",
+        body: "Inside the attic of a one-story gray house in a Seattle suburb last week, Jeff Bryson gingerly strapped copper piping across the rafters while wearing a white face mask and a headlamp. The temperature was about 110 degrees in the tight space, which was covered in insulation dust. His work was meant to cool the rest of the home."   
+    },
+    {
+    id: 7,
+    headline: "Testing2",
+    createdOn: '11/13/11',
+    author:"beginner2",
+    image: 134,
+    summary: "Triple-digit temperatures led to a spike in demand across the region.",
+    body: "Inside the attic of a one-story gray house in a Seattle suburb last week, Jeff Bryson gingerly strapped copper piping across the rafters while wearing a white face mask and a headlamp. The temperature was about 110 degrees in the tight space, which was covered in insulation dust. His work was meant to cool the rest of the home."   
+    }
+]
 
 test("renders zero articles without errors", async () => {
+    // mockArticleService.mockResolvedValueOnce(testArticlesNone);
+    // render(<View/>)
+    // await waitFor(()=>{
+    //     const articles= screen.queryAllByText(/by/i);
+    //     console.log(articles);
+    // })
+
 });
 
 // test("renders three articles without errors", async ()=> {
+//     mockArticleService.mockResolvedValueOnce(testArticles);
+//     render(<View/>)
 // });
 
 //Task List
